069beb3924a451f2ecc5a23a0bda1adb5da7a89a
smartyshko <smartyshko@qualium-systems.com.ua>
2018-03-13 14:17:07 +0200

    home page

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+	$( ".cross" ).hide();
+	$( ".menu" ).hide();
+	$( ".hamburger" ).click(function() {
+		$( ".header-menu" ).slideToggle( "slow", function() {
+			$( ".hamburger" ).hide();
+			$( ".cross" ).show();
+		});
+	});
+
+	$( ".cross" ).click(function() {
+		$( ".header-menu" ).slideToggle( "slow", function() {
+			$( ".cross" ).hide();
+			$( ".hamburger" ).show();
+		});
+	});
+
+});	
+
