6683e338f11898366e7c3446145ee512a2d4cb75
Vlada <you@example.com>
2018-03-27 09:27:27 +0300

    add new page

--- models/FileUpload.js
+++ models/FileUpload.js
@@ -4,7 +4,7 @@ var FileUpload = new keystone.List('FileUpload');
 var myStorage = new keystone.Storage({
 	adapter: keystone.Storage.Adapters.FS,
 	fs: {
-		path: keystone.expandPath('./public/uploads/files'), 
+		path: keystone.expandPath('./public/uploads/files'),
 		publicPath: '/public/uploads/files',
 	},
 });
@@ -22,9 +22,9 @@ FileUpload.add({
 	ans_5: { type: Number },
 	day: { type: Number },
 	language: { type: String },
-	subscribe: { type: String },
+	subscribe:  { type: String },
 	createdAt: { type: Date, default: Date.now },
 });
 
-FileUpload.defaultColumns = 'name,first_name, last_name,email,phone,age,ans_1,ans_2,ans_3,ans_4,ans_5,language,subscribe,createdAt';
+FileUpload.defaultColumns = 'name,first_name, last_name,email,phone,age,ans_1,ans_2,ans_3,ans_4,ans_5, language,subscribe,createdAt';
 FileUpload.register();
