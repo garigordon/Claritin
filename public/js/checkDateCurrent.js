b8fcced91c95646d5ea56bd4bb98993060a639d2
Vlada <you@example.com>
2018-04-12 10:52:35 +0300

    reCommit

---
+++ public/js/checkDateCurrent.js
@@ -0,0 +1,13 @@
+var day = new Date();
+var days = day.getDate();
+var month = day.getMonth() + 1;
+var $lox1 = document.getElementById('lox1');
+var $lox2 = document.getElementById('lox2');
+if (month === 4 && days >= 16) {
+	$lox1.innerHTML = $lox1.innerHTML;
+} else if (month === 5 && days <= 25) {
+	$lox1.innerHTML = $lox1.innerHTML;
+} else {
+	$lox1.innerHTML = $lox2.innerHTML;
+	$lox1.classList.add("header-menu__list--right");
+}
